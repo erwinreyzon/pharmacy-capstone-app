46a10dd428bd71b41edc6e42c5945d4be878c1b7
erwin <winzon353535@gmail.com>
2022-04-21 12:09:50 -0700

    adds route and action for drug show

--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -5,4 +5,9 @@ class DrugsController < ApplicationController
     render json: drugs
   end
 
+  def show
+    drug = Drug.find_by(id: params[:id])
+    render json: drug
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   get "/drugs" => "drugs#index"
+  get "/drugs/:id" => "drugs#show"
 
 end
