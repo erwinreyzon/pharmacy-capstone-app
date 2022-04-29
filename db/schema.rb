fc98e7dd197af311a66f1661a3233780a038b928
erwin <winzon353535@gmail.com>
2022-04-29 13:15:11 -0700

    adds user model and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_29_191518) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_29_201441) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_29_191518) do
     t.string "description"
     t.integer "classification_id"
     t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
