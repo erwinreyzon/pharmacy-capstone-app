d283817ecf958ae5e281a6d9fad886688a0275b2
erwin <winzon353535@gmail.com>
2022-05-09 11:20:19 -0700

    adds assossiations between drug, user, and drug_user

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,3 +1,5 @@
 class Drug < ApplicationRecord
   belongs_to :classification
+  has_many :drug_users
+  has_many :users, through: :drug_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  has_many :drug_users
+  has_many :drugs, through: :drug_users
 end
