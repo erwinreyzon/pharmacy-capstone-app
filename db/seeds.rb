7573402f915cc38b03dc2d5f8bbef9dd159319af
erwin <winzon353535@gmail.com>
2022-05-30 14:26:03 -0700

    adds seed info for Hydrocodoen-APAP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Drug.create(name: "Hydrocode-APAP", description: "Hydrocodone and acetaminophen combination is used to relieve pain severe enough to require opioid treatment and when other pain medicines did not work well enough or cannot be tolerated.
+
+  Acetaminophen is used to relieve pain and reduce fever in patients. It does not become habit-forming when taken for a long time. But acetaminophen may cause other unwanted effects when taken in large doses, including liver damage.", classification_id: 1, image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.webmd.com%2Fdrugs%2F2%2Fdrug-251%2Fhydrocodone-acetaminophen-oral%2Fdetails&psig=AOvVaw30kD-7HAmLjF756oL3E8R0&ust=1654032177668000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMCTipeUiPgCFQAAAAAdAAAAABAD")
+Drug.create(name:, description:, classification_id:, image_url:)
+Drug.create(name:, description:, classification_id:, image_url:)
+Drug.create(name:, description:, classification_id:, image_url:)
+Drug.create(name:, description:, classification_id:, image_url:)
+Drug.create(name:, description:, classification_id:, image_url:)
+Drug.create(name:, description:, classification_id:, image_url:)
+Drug.create(name:, description:, classification_id:, image_url:)
+Drug.create(name:, description:, classification_id:, image_url:)
+Drug.create(name:, description:, classification_id:, image_url:)
